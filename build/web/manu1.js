eda2e82c7c3159b81dda992f02b1abf0392407fd
leo566491 <wutunghan@WuTungs-MacBook-Air.local>
2016-04-26 12:15:50 +0800

    this a CossiePlay
    This Project display detail about the cosplay

---
+++ build/web/manu1.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write(' <link rel="stylesheet" type="text/css" href="style.css"/>');
+document.write('<div class="headphoto">');
+document.write('<h2 class="headtext2">cutom cosplay since 2012</h2>');
+document.write('<h2 class="headtext"><a href="index.html">CossiePlay</h2></a></div>');
+document.write(' <div class="tablebar"><table bgcolor="#A9F5E1" border="3" style="width:100%" ><tr>');
+document.write(' <td><a href="about.html">About</a></td>');
+document.write(' <td><a href="product.html">Product</a></td>');
+document.write(' <td><a href="guide.html">Guideline</a></td>');
+document.write(' <td><a href="announce.html">Announcement</a></td>');
+document.write(' <td><a href="signup.html">Sign Up</a></td>');
+document.write(' <br/></div></table>');
+
---
+++ build/web/manu2.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write(' <link rel="stylesheet" type="text/css" href="style.css"/>');
+document.write(' <div class="buttonmanu"><table bgcolor="#A9F5E1" border="1" style="width:100%"> <tr>');
+document.write(' <td><a href="contact.html">Contact Us</a></td>');
+document.write(' <td><a href="link.html">Links</a></td>');
+document.write(' </table></tr></div>');
+document.write(' <footer class="footer" id="footer">@ 2016, CossiePlay. All Rights Reserved.</footer>"');
